65a2f77abdba71f2adce0d663f6951b8e26d1998
Proudmoor <zpf238@gmail.com>
2015-01-15 01:23:42 +0800

    add key control

--- SoftRasterizer/main.cpp
+++ SoftRasterizer/main.cpp
@@ -56,8 +56,7 @@ void DrawCircle(int xc, int yc, int r) {
 
 void DrawLine(int x0, int y0,  int xEnd,  int yEnd) {
     const bool steep = (fabs(yEnd - y0) > fabs(xEnd - x0)); //whether the slope greater than 1
-    // const bool horz = fabs(yEnd - y0); // whether is a horizontal line
-    // const bool vert = fabs (xEnd - x0); // whether is a vertical line
+    
     if (steep) {
         std::swap(x0, y0);
         std::swap(xEnd, yEnd);
@@ -115,7 +114,8 @@ int main(int argc, char** argv) {
     //initialize memory to ....uint8
     memset(pixels, 255, 640 * 480 * sizeof(Uint32));
     
-    
+    int drawline = 0;
+    int drawcircle = 0;
     bool ptCount = false; // whether have a point
     while (!quit) {
         SDL_UpdateTexture(texture, NULL, pixels, 640*sizeof(Uint32));
@@ -124,6 +124,18 @@ int main(int argc, char** argv) {
         int x2, y2;
         
         switch(event.type){
+            case SDL_KEYDOWN:
+                switch (event.key.keysym.sym){
+                case SDLK_1:
+                    drawline = 1;
+                    drawcircle = 0;
+                    break;
+                case SDLK_2:
+                    drawcircle = 1;
+                    drawline = 0;
+                    break;
+                }
+    
             case SDL_MOUSEBUTTONUP:
                 if (event.button.button == SDL_BUTTON_LEFT)
                     leftMouseButtonDown = false;
@@ -132,6 +144,7 @@ int main(int argc, char** argv) {
             case SDL_MOUSEBUTTONDOWN:
                 if (event.button.button == SDL_BUTTON_LEFT){
                     leftMouseButtonDown = true;
+                    if (leftMouseButtonDown == true){
                     if(ptCount == 0){
                         SDL_GetMouseState(&x1, &y1);
                         std::stringstream ss;
@@ -145,11 +158,13 @@ int main(int argc, char** argv) {
                         ss << "X2:" << x2 << "  X2:" << y2;
                         SDL_SetWindowTitle(window, ss.str().c_str());
                         ptCount = false;
-                        DrawLine(x1, y1, x2, y2);
                         double r  = sqrt(double(((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2))));
-                        DrawCircle(x1, y1, int (r));
+                        if (drawline)  DrawLine(x1, y1, x2, y2);
+                        if (drawcircle)  DrawCircle(x1, y1, int (r));
                     }
                 }
+            }
+                
             case SDL_MOUSEMOTION:
                 if (leftMouseButtonDown){
                     //int mouseX = event.motion.x;
