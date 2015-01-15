f8843101643e06291d5ec5f5f98e84f820194957
Proudmoor <zpf238@gmail.com>
2015-01-15 20:36:57 +0800

    add vlm

---
+++ softRender/softRender/Pixels.cpp
@@ -0,0 +1,9 @@
+//
+//  Pixels.cpp
+//  softRender
+//
+//  Created by zpf on 15-1-15.
+//  Copyright (c) 2015年 zpf. All rights reserved.
+//
+
+#include "Pixels.h"
---
+++ softRender/softRender/Pixels.h
@@ -0,0 +1,16 @@
+//
+//  Pixels.h
+//  softRender
+//
+//  Created by zpf on 15-1-15.
+//  Copyright (c) 2015年 zpf. All rights reserved.
+//
+
+#ifndef __softRender__Pixels__
+#define __softRender__Pixels__
+
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include "glm/"
+
+#endif /* defined(__softRender__Pixels__) */
